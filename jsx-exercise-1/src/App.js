170d9c6fbe0f33d2baec7b12911348c8555dbc5d
Sabrina <144979434+realvegetablegummy@users.noreply.github.com>
2025-03-07 12:39:38 -0700

    added exercise 1 files out of zip

---
+++ jsx-exercise-1/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  var currentYear = new Date().getFullYear();
+  var isLoggedIn = false;
+  return (
+    <div className="App">
+    <h1>ENSF-381: Full Stack Web Development</h1>
+    <p>React Components</p>
+    <p>{currentYear}</p>
+    <p>{isLoggedIn ? <p>Welcome, user!</p> : <p>Please log in</p>} </p>
+    </div>
+  );
+}
+
+export default App;
